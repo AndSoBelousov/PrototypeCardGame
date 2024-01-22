bc3921f424ffaaad09c71199ca25afc472d55c5a
Andrey <124515352+AndSoBelousov@users.noreply.github.com>
2024-01-23 00:20:35 +0300

    Revert "Внесены правки системы выбора героя"
    This reverts commit 36de7e1e28fde5207ecb27391e887cf8d66aa6fd.

--- Assets/Cards/Scripts/ChoosingHero.cs
+++ Assets/Cards/Scripts/ChoosingHero.cs
@@ -16,43 +16,35 @@ public class ChoosingHero : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     private Sprite _currentSprite;
 
     private Vector3 _panelOne = new Vector3(-365, 70, 0);
-    private Vector3 _panelTwo = new Vector3(-365, -115, 0);
+    private Vector3 _panelTwo = new Vector3(-365, -70, 0);
 
-    private HerosManager _herosManager;
+    private bool _isPlayerOne = true;//!!!!!!!!!!!!! меняется только у текущей цели, понять как поменять у всех 
 
     private void Start()
     {
-        
+        if(_heroOnePlayer != null && _heroTwoPlayer != null)
+        {
+            //пока ничего
+        }
     }
-
     public void OnPointerClick(PointerEventData eventData)
     {
-        _herosManager = _heroOnePlayer.GetComponent<HerosManager>();
-
-        if (_herosManager != null )
+        if (_isPlayerOne == true) 
         {
-            if (_herosManager.IsHeroOneSelect != true)
-            {
-
-                _heroOnePlayer.GetComponent<SpriteRenderer>().sprite = _currentSprite;
-
-                StartCoroutine(MovingIcon(eventData.pointerClick));
-                Debug.Log("first click");
-                _heroOnePlayer.GetComponent<HerosManager>().IsHeroOneSelect = true;
-            }
-            else
-            {
-                Debug.Log("next click");
-                _heroTwoPlayer.GetComponent<SpriteRenderer>().sprite = _currentSprite;
-
-                StartCoroutine(MovingIcon(eventData.pointerClick));
-                //СДЕЛАТЬ --- Закрытие UI панелей и переход к игре
-            }
-
+            
+            _heroOnePlayer.GetComponent<SpriteRenderer>().sprite = _currentSprite;
+            
+            StartCoroutine(MovingIcon(eventData.pointerClick));
+            Debug.Log("first click");
         }
+        else
+        {
+            Debug.Log("next click");
+            _heroTwoPlayer.GetComponent<SpriteRenderer>().sprite = _currentSprite;
 
-
-
+            StartCoroutine(MovingIcon(eventData.pointerClick));
+            
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -72,7 +64,7 @@ public class ChoosingHero : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     private IEnumerator MovingIcon(GameObject icon)
     {
         Vector3 startPos = icon.transform.localPosition;
-        Vector3 endPos = _herosManager.IsHeroOneSelect == false ? _panelOne : _panelTwo;
+        Vector3 endPos = _isPlayerOne == true ? _panelOne : _panelTwo;
 
         float time = 0;
         while(time < 1)
